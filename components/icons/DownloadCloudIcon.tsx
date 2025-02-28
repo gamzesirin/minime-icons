1891fa0d15dba3fffa23c2d4f523d3bd2e6d61ab
Gamze Şirin <gamze1sirin@gmail.com>
2025-02-28 09:49:42 +0300

    feat : added download icon component

---
+++ components/icons/DownloadCloudIcon.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const DownloadCloudIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<polyline points="8 17 12 21 16 17" />
+			<line x1="12" y1="12" x2="12" y2="21" />
+			<path d="M20.88 18.09A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.29" />
+		</svg>
+	)
+}
