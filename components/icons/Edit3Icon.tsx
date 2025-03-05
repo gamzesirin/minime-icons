5043ddf28a16330d393b548f1be53872e14c5426
Gamze Şirin <gamze1sirin@gmail.com>
2025-03-05 14:38:10 +0300

    feat: add icons components

---
+++ components/icons/Edit3Icon.tsx
@@ -0,0 +1,19 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const Edit3Icon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M12 20h9" />
+			<path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z" />
+		</svg>
+	)
+}
