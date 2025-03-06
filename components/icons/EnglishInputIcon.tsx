0dfb9e73d152965467492e3207cf42dcc5f57298
Gamze Şirin <gamze1sirin@gmail.com>
2025-03-06 11:21:57 +0300

    feat : add icon componnets

---
+++ components/icons/EnglishInputIcon.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const EnglishInputIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M4 6h16a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2z" />
+			<path d="M7 10h2m2 0h2m2 0h2" />
+			<path d="M12 14h.01" />
+		</svg>
+	)
+}
