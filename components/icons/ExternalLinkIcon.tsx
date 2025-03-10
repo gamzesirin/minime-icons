2d59c91dc4ad13b5f1d3d7c90d80e4e85397ae7e
Gamze Şirin <gamze1sirin@gmail.com>
2025-03-10 09:43:18 +0300

    feat: add icons components

---
+++ components/icons/ExternalLinkIcon.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const ExternalLinkIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+			<polyline points="15 3 21 3 21 9" />
+			<line x1="10" y1="14" x2="21" y2="3" />
+		</svg>
+	)
+}
