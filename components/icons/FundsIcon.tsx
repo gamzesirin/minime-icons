f53913141aaf9dfe07148279e945a4c96b8aacde
Gamze Şirin <gamze1sirin@gmail.com>
2025-03-11 09:31:43 +0300

    feat: add icon components

---
+++ components/icons/FundsIcon.tsx
@@ -0,0 +1,18 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const FundsIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M2 20h20M5 20V8h4m5 12V4h4" />
+		</svg>
+	)
+}
