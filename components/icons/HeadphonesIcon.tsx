4f7eec5cbb4eb344d72277c3dcc3152b562f4bb0
Gamze Şirin <gamze1sirin@gmail.com>
2025-03-13 11:38:00 +0300

    feat : add icon components

---
+++ components/icons/HeadphonesIcon.tsx
@@ -0,0 +1,19 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const HeadphonesIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M3 18v-6a9 9 0 0 1 18 0v6" />
+			<path d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3zM3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z" />
+		</svg>
+	)
+}
