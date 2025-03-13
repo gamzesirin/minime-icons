3fab9dc53890d4f3eef18bfd3b1e4e5ce79280dd
Gamze Şirin <gamze1sirin@gmail.com>
2025-03-13 11:37:54 +0300

    feat : add icon components

---
+++ components/icons/HashIcon.tsx
@@ -0,0 +1,21 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const HashIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<line x1="4" y1="9" x2="20" y2="9" />
+			<line x1="4" y1="15" x2="20" y2="15" />
+			<line x1="10" y1="3" x2="8" y2="21" />
+			<line x1="16" y1="3" x2="14" y2="21" />
+		</svg>
+	)
+}
