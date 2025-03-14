4e2480d0d3c426adb14480eafe8313cb16c9a761
Gamze Şirin <gamze1sirin@gmail.com>
2025-03-14 11:33:22 +0300

    feat : add icon component

---
+++ components/icons/HexagonIcon.tsx
@@ -0,0 +1,18 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const HexagonIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" />
+		</svg>
+	)
+}
