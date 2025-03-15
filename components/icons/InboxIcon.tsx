5d6a8dd057fed4898b17ea65f8be0e51793012c8
Gamze Şirin <gamze1sirin@gmail.com>
2025-03-15 18:48:41 +0300

    feat: add icons components

---
+++ components/icons/InboxIcon.tsx
@@ -0,0 +1,19 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const InboxIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M22 12h-6l-2 3h-4l-2-3H2" />
+			<path d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z" />
+		</svg>
+	)
+}
---
+++ components/icons/InfoIcon.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const InfoIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<circle cx="12" cy="12" r="10" />
+			<line x1="12" y1="16" x2="12" y2="12" />
+			<line x1="12" y1="8" x2="12.01" y2="8" />
+		</svg>
+	)
+}
