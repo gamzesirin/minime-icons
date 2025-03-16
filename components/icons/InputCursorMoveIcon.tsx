543dffa7ce8b5623b15961eda9c80f8e0626fd19
Gamze Şirin <gamze1sirin@gmail.com>
2025-03-16 20:14:14 +0300

    feat: add icon component

---
+++ components/icons/InputCursorMoveIcon.tsx
@@ -0,0 +1,19 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const InputCursorMoveIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M5 9h14M5 15h14" />
+			<path d="M13 5l4 4-4 4M11 19l-4-4 4-4" />
+		</svg>
+	)
+}
