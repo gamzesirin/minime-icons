3576f831c4623db1342fa8056db859a82cae4a19
Gamze Şirin <gamze1sirin@gmail.com>
2025-03-17 15:56:32 +0300

    feat : add icon components

---
+++ components/icons/ItalicIcon.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const ItalicIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<line x1="19" y1="4" x2="10" y2="4" />
+			<line x1="14" y1="20" x2="5" y2="20" />
+			<line x1="15" y1="4" x2="9" y2="20" />
+		</svg>
+	)
+}
