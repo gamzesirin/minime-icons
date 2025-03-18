cbf2de02917403301ad842676cd6780a63dc4882
Gamze Şirin <gamze1sirin@gmail.com>
2025-03-18 09:53:16 +0300

    feat: add icon component

---
+++ components/icons/LayersIcon.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const LayersIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<polygon points="12 2 2 7 12 12 22 7 12 2" />
+			<polyline points="2 17 12 22 22 17" />
+			<polyline points="2 12 12 17 22 12" />
+		</svg>
+	)
+}
