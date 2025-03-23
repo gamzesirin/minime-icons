623db57506f630c01947e7a454f0ed75d7833b69
Gamze Şirin <gamze1sirin@gmail.com>
2025-03-23 21:19:52 +0300

    feat: add icon component

---
+++ components/icons/ListIcon.tsx
@@ -0,0 +1,23 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const ListIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<line x1="8" y1="6" x2="21" y2="6" />
+			<line x1="8" y1="12" x2="21" y2="12" />
+			<line x1="8" y1="18" x2="21" y2="18" />
+			<line x1="3" y1="6" x2="3.01" y2="6" />
+			<line x1="3" y1="12" x2="3.01" y2="12" />
+			<line x1="3" y1="18" x2="3.01" y2="18" />
+		</svg>
+	)
+}
