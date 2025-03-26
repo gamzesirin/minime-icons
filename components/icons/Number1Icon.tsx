ab6daf1762675e70448bf177c0f12fde382787d9
Gamze Şirin <gamze1sirin@gmail.com>
2025-03-26 13:38:04 +0300

    feat: add icon components

---
+++ components/icons/Number1Icon.tsx
@@ -0,0 +1,18 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const Number1Icon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M12 4v16M8 8l4-4 4 4" />
+		</svg>
+	)
+}
