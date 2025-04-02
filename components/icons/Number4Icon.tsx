080a0bf192c296c60983ca9db4032ba1db1f1045
Gamze Şirin <gamze1sirin@gmail.com>
2025-04-02 12:43:32 +0300

    feat: add icon components

---
+++ components/icons/Number4Icon.tsx
@@ -0,0 +1,18 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const Number4Icon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M15 4v16M8 4l7 8H4" />
+		</svg>
+	)
+}
