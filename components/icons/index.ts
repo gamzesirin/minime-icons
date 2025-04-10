cd4d8833e5e37a1529fa24aec012a93acb3a25d4
Gamze Şirin <gamze1sirin@gmail.com>
2025-04-10 12:55:14 +0300

    feat: add icon index page

---
+++ components/icons/index.ts
@@ -0,0 +1,59 @@
+// Number Icons
+export { Number0Icon } from './Number0Icon'
+export { Number1Icon } from './Number1Icon'
+export { Number2Icon } from './Number2Icon'
+export { Number3Icon } from './Number3Icon'
+export { Number4Icon } from './Number4Icon'
+export { Number5Icon } from './Number5Icon'
+export { Number6Icon } from './Number6Icon'
+export { Number7Icon } from './Number7Icon'
+export { Number8Icon } from './Number8Icon'
+export { Number9Icon } from './Number9Icon'
+
+// Interface Icons
+export { AbIcon } from './AbIcon'
+export { CloseIcon } from './CloseIcon'
+export { CodeIcon } from './CodeIcon'
+export { CursorClickIcon } from './CursorClickIcon'
+export { EditIcon } from './EditIcon'
+export { FileIcon } from './FileIcon'
+export { HomeIcon } from './HomeIcon'
+export { ImageIcon } from './ImageIcon'
+export { MenuIcon } from './MenuIcon'
+export { SearchIcon } from './SearchIcon'
+export { SettingsIcon } from './SettingsIcon'
+
+// Media Icons
+export { MusicIcon } from './MusicIcon'
+export { PauseIcon } from './PauseIcon'
+export { PlayIcon } from './PlayIcon'
+export { StopIcon } from './StopIcon'
+export { VideoIcon } from './VideoIcon'
+
+// Business Icons
+export { CashIcon } from './CashIcon'
+export { CoinIcon } from './CoinIcon'
+export { CreditCardIcon } from './CreditCardIcon'
+export { DollarSignIcon } from './DollarSignIcon'
+export { FundsIcon } from './FundsIcon'
+export { GiftIcon } from './GiftIcon'
+export { MoneyDollarBoxIcon } from './MoneyDollarBoxIcon'
+export { MoneyDollarCircleIcon } from './MoneyDollarCircleIcon'
+export { MoneyEuroBoxIcon } from './MoneyEuroBoxIcon'
+export { MoneyEuroCircleIcon } from './MoneyEuroCircleIcon'
+export { ShoppingCartIcon } from './ShoppingCartIcon'
+
+// Social Icons
+export { DribbbleIcon } from './DribbbleIcon'
+export { GithubIcon } from './GithubIcon'
+export { InstagramIcon } from './InstagramIcon'
+export { LinkedinIcon } from './LinkedinIcon'
+export { TwitterIcon } from './TwitterIcon'
+
+// Other Icons
+export { HeartIcon } from './HeartIcon'
+export { SparklesIcon } from './SparklesIcon'
+export { StarIcon } from './StarIcon'
+export { TagIcon } from './TagIcon'
+export { TrashIcon } from './TrashIcon'
+export { UsersIcon } from './UsersIcon'
