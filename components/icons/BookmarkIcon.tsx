8d1717c45c110e26091d9869f500ad2a67f30ffa
Gamze Şirin <gamze1sirin@gmail.com>
2025-04-24 22:29:16 +0300

    feat (icons): added icon components

---
+++ components/icons/BookmarkIcon.tsx
@@ -0,0 +1,18 @@
+import { IconProps } from '@/types/Icon-type'
+
+export const BookmarkIcon = ({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) => {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+		</svg>
+	)
+}
