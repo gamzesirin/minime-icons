e588ebca766e6391da47247be0dd9f17ba0271f0
Gamze Şirin <gamze1sirin@gmail.com>
2025-09-28 13:45:43 +0300

    feat(icons): Added Plane, Receipt, Refresh ve Send icons

---
+++ components/icons/ReceiptIcon.tsx
@@ -0,0 +1,21 @@
+import { IconProps } from '@/types/Icon-type'
+
+export function ReceiptIcon({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path d="M4 2v20l2-1 2 1 2-1 2 1 2-1 2 1 2-1 2 1V2l-2 1-2-1-2 1-2-1-2 1-2-1-2 1-2-1Z" />
+			<path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8" />
+			<path d="M12 6V4" />
+			<path d="M12 20v-2" />
+		</svg>
+	)
+}
---
+++ components/icons/RefreshIcon.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from '@/types/Icon-type'
+
+export function RefreshIcon({ size = 24, color = 'currentColor', strokeWidth = 2 }: IconProps) {
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke={color}
+			strokeWidth={strokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<polyline points="23 4 23 10 17 10" />
+			<polyline points="1 20 1 14 7 14" />
+			<path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15" />
+		</svg>
+	)
+}
